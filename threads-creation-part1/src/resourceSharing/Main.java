a5798dbc5e580c97916abfe2b4cbbdba4530e39d
evgen <parom.evgen@mail.ru>
2024-03-23 12:08:42 +0300

    16. Resource Sharing & Introduction to Critical Sections

---
+++ threads-creation-part1/src/resourceSharing/Main.java
@@ -0,0 +1,70 @@
+package resourceSharing;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        InventoryCounter inventoryCounter = new InventoryCounter();
+
+        IncrementThread incrementThread = new IncrementThread(inventoryCounter);
+        DecrementingThread decrementingThread = new DecrementingThread(inventoryCounter);
+
+//        // works not parallel and normal
+//        incrementThread.start();
+//        incrementThread.join();
+//        decrementingThread.start();
+//        decrementingThread.join();
+
+        // works parallel and NOT normal
+        incrementThread.start();
+        decrementingThread.start();
+        decrementingThread.join();
+        incrementThread.join();
+
+        System.out.println("We currently have " + inventoryCounter.getItems() + " items");
+    }
+
+    public static class IncrementThread extends Thread {
+        private InventoryCounter inventoryCounter;
+
+        public IncrementThread(InventoryCounter inventoryCounter) {
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                inventoryCounter.increment();
+            }
+        }
+    }
+
+    public static class DecrementingThread extends Thread {
+        private InventoryCounter inventoryCounter;
+
+        public DecrementingThread(InventoryCounter inventoryCounter) {
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                inventoryCounter.decrement();
+            }
+        }
+    }
+
+    private static class InventoryCounter {
+        private int items = 0;
+
+        public void increment() {
+            items++;
+        }
+
+        public void decrement() {
+            items--;
+        }
+
+        public int getItems() {
+            return items;
+        }
+    }
+}
