f4a8890e90233cf6c55964b7eafe86b58ff7f4cb
parom <E.Parominskiy@isida.by>
2024-03-25 15:23:22 +0300

    25. Product Reviews Service - Solution

---
+++ threads-creation-part1/src/readWriteLockTestTask/ProductReviewsService.java
@@ -0,0 +1,171 @@
+package readWriteLockTestTask;
+
+/**
+ * @author E.Parominsky 25/03/2024 15:20
+ */
+import java.util.*;
+import java.util.concurrent.locks.*;
+
+public class ProductReviewsService {
+    private final HashMap<Integer, List<String>> productIdToReviews;
+    private ReentrantReadWriteLock locks = new ReentrantReadWriteLock();
+
+    // Create your member variables here
+
+
+
+    /********* DO NOT MODIFY THIS SECTION **************/
+
+    public ProductReviewsService() {
+        this.productIdToReviews = new HashMap<>();
+    }
+
+    /**
+     * Adds a product ID if not present
+     */
+    public void addProduct(int productId) {
+        Lock lock = getLockForAddProduct();
+
+        lock.lock();
+
+        try {
+            if (!productIdToReviews.containsKey(productId)) {
+                productIdToReviews.put(productId, new ArrayList<>());
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    /**
+     * Removes a product by ID if present
+     */
+    public void removeProduct(int productId) {
+        Lock lock = getLockForRemoveProduct();
+
+        lock.lock();
+
+        try {
+            if (productIdToReviews.containsKey(productId)) {
+                productIdToReviews.remove(productId);
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    /**
+     * Adds a new review to a product
+     * @param productId - existing or new product ID
+     * @param review - text containing the product review
+     */
+    public void addProductReview(int productId, String review) {
+        Lock lock = getLockForAddProductReview();
+
+        lock.lock();
+
+        try {
+            if (!productIdToReviews.containsKey(productId)) {
+                productIdToReviews.put(productId, new ArrayList<>());
+            }
+            productIdToReviews.get(productId).add(review);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    /**
+     * Returns all the reviews for a given product
+     */
+    public List<String> getAllProductReviews(int productId) {
+        Lock lock = getLockForGetAllProductReviews();
+
+        lock.lock();
+
+        try {
+            if (productIdToReviews.containsKey(productId)) {
+                return Collections.unmodifiableList(productIdToReviews.get(productId));
+            }
+        } finally {
+            lock.unlock();
+        }
+
+        return Collections.emptyList();
+    }
+
+    /**
+     * Returns the latest review for a product by product ID
+     */
+    public Optional<String> getLatestReview(int productId) {
+        Lock lock = getLockForGetLatestReview();
+
+        lock.lock();
+
+        try {
+
+            if (productIdToReviews.containsKey(productId) && !productIdToReviews.get(productId).isEmpty()) {
+                List<String> reviews = productIdToReviews.get(productId);
+                return Optional.of(reviews.get(reviews.size() - 1));
+            }
+        } finally {
+            lock.unlock();
+        }
+
+        return Optional.empty();
+    }
+
+    /**
+     * Returns all the product IDs that contain reviews
+     */
+    public Set<Integer> getAllProductIdsWithReviews() {
+        Lock lock = getLockForGetAllProductIdsWithReviews();
+
+        lock.lock();
+
+        try {
+            Set<Integer> productsWithReviews = new HashSet<>();
+            for (Map.Entry<Integer, List<String>> productEntry : productIdToReviews.entrySet()) {
+                if (!productEntry.getValue().isEmpty()) {
+                    productsWithReviews.add(productEntry.getKey());
+                }
+            }
+            return productsWithReviews;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    /********* END OF UNMODIFIABLE SECTION **************/
+
+
+    Lock getLockForAddProduct() {
+        return    this.locks.writeLock();
+        // Add code here
+    }
+
+    Lock getLockForRemoveProduct() {
+        return    this.locks.writeLock();
+        // add code here
+    }
+
+    Lock getLockForAddProductReview() {
+        return    this.locks.writeLock();
+        // add code here
+    }
+
+    Lock getLockForGetAllProductReviews() {
+        return    this.locks.readLock();
+        // add code here
+    }
+
+    Lock getLockForGetLatestReview() {
+        return    this.locks.readLock();
+        // add code here
+    }
+
+    Lock getLockForGetAllProductIdsWithReviews() {
+        return    this.locks.readLock();
+        // add code here
+    }
+}
+
